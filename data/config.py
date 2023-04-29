c764f82dae5327a3f6def07449b77e95b6e44322
Aidar Rakhmatullin <nedopolz@gmail.com>
2023-04-29 17:43:26 +0400

    add new admin

--- data/config.py
+++ data/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-admins = ["419519710"]
+admins = ["419519710", str(os.getenv("NEWADMIN"))]
 BOT_TOKEN = str(os.getenv("BOT_TOKEN"))
 
 NOTION_API_KEY = str(os.getenv("NOTION_API_KEY"))
